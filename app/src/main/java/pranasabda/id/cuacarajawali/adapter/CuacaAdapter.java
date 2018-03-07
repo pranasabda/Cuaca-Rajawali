74be28ccc97162592b07b36bf66714bad4aff78f
pranasabda <pranasabda@gmail.com>
2018-03-07 12:03:38 +0700

    Fixed list  Icon Weather

--- app/src/main/java/pranasabda/id/cuacarajawali/adapter/CuacaAdapter.java
+++ app/src/main/java/pranasabda/id/cuacarajawali/adapter/CuacaAdapter.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -39,6 +41,7 @@ public class CuacaAdapter extends ArrayAdapter<Cuaca> {
         TextView weather = (TextView) rowView.findViewById(R.id.tv_cuaca);
         TextView temp = (TextView) rowView.findViewById(R.id.tv_temp_detail);
         TextView humadity = (TextView) rowView.findViewById(R.id.tv_humadity_detail);
+        ImageView detailImage = (ImageView) rowView.findViewById(R.id.img_cuaca_detail);
 
         //nampilin data / siap disajikan
 //        dt.setText(cuaca[position].getDt());
@@ -50,6 +53,21 @@ public class CuacaAdapter extends ArrayAdapter<Cuaca> {
         weather.setText(cuaca.get(position).getWeather());
         temp.setText(cuaca.get(position).getTemp());
         humadity.setText(cuaca.get(position).getHumadity());
+
+        if (weather.getText().toString().equals("Clear")){
+            detailImage.setImageResource(R.drawable.ic_011_sun);
+        } else if (weather.getText().toString().equals("Clouds")){
+            detailImage.setImageResource((R.drawable.ic_009_cloudy));
+        }else if (weather.getText().toString().equals("light_clouds")){
+            detailImage.setImageResource((R.drawable.ic_008_cloud));
+        }else if (weather.getText().toString().equals("Rain")){
+            detailImage.setImageResource((R.drawable.ic_006_raining));
+        }else if (weather.getText().toString().equals("Wind")){
+            detailImage.setImageResource((R.drawable.ic_003_wind));
+        }else {
+//            Toast.makeText(this, "Eureka", Toast.LENGTH_SHORT).show();
+            detailImage.setImageResource(R.drawable.ic_001_wind_1);
+        }
 
 
         return rowView;
